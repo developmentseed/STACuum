ef1ae4e79c4c3a6c5be574643c53844f53475328
ingalls <nick@ingalls.ca>
2023-09-08 14:42:14 +0100

    Overwrite Location

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    },
     plugins: [
         vue()
     ],
