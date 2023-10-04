a224eab674495efc8c506b14f63a86b5f2cf8e81
ingalls <nick@ingalls.ca>
2023-10-04 11:41:40 -0600

    Fix syntax

--- cloudformation/lib/dynamo.js
+++ cloudformation/lib/dynamo.js
@@ -14,10 +14,6 @@ export default {
                     AttributeName: 'ServerUrl',
                     KeyType: 'HASH'
                 }],
-                ProvisionedThroughput: {
-                    ReadCapacityUnits: 5,
-                    WriteCapacityUnits: 5
-                }
             }
         },
         DDBServicesTable: {
@@ -32,73 +28,7 @@ export default {
                     AttributeName: 'ServiceName',
                     KeyType: 'HASH'
                 }],
-                ProvisionedThroughput: {
-                    ReadCapacityUnits: 5,
-                    WriteCapacityUnits: 5
-                }
             }
         },
-        DDBWriteCapacityScalableTarget: {
-            Type: 'AWS::ApplicationAutoScaling::ScalableTarget',
-            Properties: {
-                MaxCapacity: 30,
-                MinCapacity: 5,
-                ResourceId: cf.join('/', ['table', cf.ref('DDBTable')]),
-                RoleARN: cf.getAtt('DDBScalingRole', 'Arn'),
-                ScalableDimension: 'dynamodb:table:WriteCapacityUnits',
-                ServiceNamespace: 'dynamodb'
-            }
-        },
-        DDBScalingRole: {
-            Type: 'AWS::IAM::Role',
-            Properties: {
-                AssumeRolePolicyDocument: {
-                    Version: '2012-10-17',
-                    Statement: [{
-                        Effect: 'Allow',
-                        Principal: {
-                            Service: ['application-autoscaling.amazonaws.com']
-                        },
-                        Action: ['sts:AssumeRole']
-                    }]
-                },
-                Path: '/',
-                Policies: [{
-                    PolicyName: 'root',
-                    PolicyDocument: {
-                        Version: '2012-10-17',
-                        Statement: [{
-                            Effect: 'Allow',
-                            Action: [
-                                'dynamodb:DescribeTable',
-                                'dynamodb:UpdateTable',
-                                'cloudwatch:PutMetricAlarm',
-                                'cloudwatch:DescribeAlarms',
-                                'cloudwatch:GetMetricStatistics',
-                                'cloudwatch:SetAlarmState',
-                                'cloudwatch:DeleteAlarms'
-                            ],
-                            Resource: '*'
-                        }]
-                    }
-                }]
-            }
-        },
-        DDBWriteScalingPolicy: {
-            Type: 'AWS::ApplicationAutoScaling::ScalingPolicy',
-            Properties: {
-                PolicyName: 'WriteAutoScalingPolicy',
-                PolicyType: 'TargetTrackingScaling',
-                ScalingTargetId: cf.ref('DDBWriteCapacityScalableTarget'),
-                TargetTrackingScalingPolicyConfiguration: {
-                    TargetValue: 75.0,
-                    ScaleInCooldown: 60,
-                    ScaleOutCooldown: 60,
-                    PredefinedMetricSpecification: {
-                        PredefinedMetricType: 'DynamoDBWriteCapacityUtilization'
-                    }
-                }
-            }
-        }
     }
 };
